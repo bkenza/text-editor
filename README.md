fd8428c160f6860d3b4ed4c7b65ddf6113d75e11
Kenza <kenza.boulisfane@gmail.com>
2021-02-28 19:39:41 -0500

    Create text editor with basic functionalities

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # text-editor
 Simple text editor built with Python and Tkinter.
+
+# Current Functionalities
+
+1. Basic text editor functionality: writing text, erasing
+2. Theme: dark and light theme
+3. Different Fonts
+4. Save file as
+
+# Technologies
+
+Python & Tkinter
+
+# How to test
+
+1. Clone the repo.
+2. Navigate into the project folder
+3. Run the command ```python3 texteditor.py```
+
---
+++ texteditor.py
@@ -0,0 +1,85 @@
+import sys
+from tkinter import *
+from tkinter import filedialog
+
+####################
+# FUNCTIONS #
+####################
+
+
+def saveas():
+    global text
+    t = text.get("1.0", "end-1c")
+    savelocation = filedialog.asksaveasfilename()
+    file1 = open(savelocation, "w+")
+    file1.write(t)
+    file1.close()
+
+
+def changetheme():
+    global text
+    text.config(background='black', foreground='white',
+                insertbackground='white')
+
+
+def FontHelvetica():
+    global text
+    text.config(font="Helvetica")
+
+
+def FontCourier():
+    global text
+    text.config(font="Courier")
+
+
+def FontArial():
+    global text
+    text.config(font="Arial")
+
+
+def FontTimes():
+    global text
+    text.config(font='Times')
+
+#########################
+    # TEXT EDITOR
+#########################
+
+
+# Create text editor
+text_editor = Tk("Kenza's text editor")
+
+# Add text widget
+text = Text(text_editor)
+text.grid()
+
+# Add save button
+button = Button(text_editor, text="Save", command=saveas)
+button.grid(row=1, column=1)
+
+
+# Add theme changer
+theme = Button(text_editor, text="Theme", command=changetheme)
+theme.grid(row=1, column=2)
+
+# Add font menu
+font = Menubutton(text_editor, text="Font")
+font.grid(row=1, column=3)
+font.menu = Menu(font, tearoff=0)
+font["menu"] = font.menu
+Helvetica = IntVar()
+Arial = IntVar()
+Times = IntVar()
+Courier = IntVar()
+
+font.menu.add_checkbutton(label="Courier", variable=Courier,
+                          command=FontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=Helvetica,
+                          command=FontHelvetica)
+font.menu.add_checkbutton(label="Arial", variable=Arial,
+                          command=FontArial)
+font.menu.add_checkbutton(label="Times", variable=Times,
+                          command=FontTimes)
+
+
+text_editor.mainloop()
